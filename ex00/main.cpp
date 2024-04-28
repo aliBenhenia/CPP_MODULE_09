808642ddd73fa489c154c3dd753128fd2b486d55
Ali ben <alibenhenia1@gmail.com>
2024-04-28 21:02:03 +0100

    init

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,44 +1,33 @@
 #include <iostream>
 #include <vector>
+#include <map>
+#include <string>
 
-std::vector<int>::iterator findClosetNumberTop(std::vector<int>data, int num)
-{
-    std::vector<int>::iterator it = data.begin();
-    while (it != data.end())
-    {
-        if (num < *it)
-            return (it);
-        it++;
-    }
-    return (data.end());// not found
-}
-int findClosetNumberBottom(std::vector<int>data, int num)
-{
-    std::vector<int>::iterator it = findClosetNumberTop(data, num);
-    if (it != data.end())
-    {
-        if (*(it - 1) == num)
-        {
-            it--;
-            it--;
-            return (*it);
-        }
-        else
-            return (*(--it));
-    }
-    return (0);
-}
 int main()
 {
-    std::vector<int>v;
-    v.push_back(1);
-    v.push_back(4);
-    v.push_back(8);
-    v.push_back(9);
-    v.push_back(19);
-    v.push_back(99);
-    v.push_back(890);
-    v.push_back(890445);
-    std::cout << findClosetNumberBottom(v, 465) << std::endl;
+    std::map<std::string, int> data;
+    data["aya"] = 9;
+    data["issam"] = 8;
+    data["ali"] = 7764;
+    data["jawad"] = 5;
+    data["emrah"] = 56;
+ 
+    std::map<std::string, int>::iterator it = data.begin();
+    // while (it != data.end())
+    // {
+    //     std::cout << "address:" << &it->first << " = " << it->second << std::endl;
+    //     it++;
+    // }
+    std::vector<char>v;
+    v.push_back('1');
+    v.push_back('3');
+    v.push_back('5');
+    v.push_back('6');
+    int i = 0;
+    while (i < v.size())
+    {
+        std::cout << "address:" << &v[i] << " = " << v[i]<< std::endl;
+        i++;
+    }
     return 0;
 }
