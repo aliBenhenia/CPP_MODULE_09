7462f4bdf71870ed78f8881fd4228e693e12c707
Ali ben <alibenhenia1@gmail.com>
2024-05-01 17:49:20 +0100

    rm struct

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -8,40 +8,6 @@
 #include <sstream>
 #include <stdexcept>// This header is part of the error handling library.
 
-struct Date {
-    int year;
-    int month;
-    int day;
-    Date()
-    {
-        this->year  = -1;
-        this->month = -1;
-        this->day   = -1;
-    }
-    Date(int yr, int mt, int dy)
-    {
-        this->year  = yr;
-        this->month = mt;
-        this->day   = dy;
-    }
-    int getYear() const {
-        return year;
-    }
-
-    int getMonth() const {
-        return month;
-    }
-
-    int getDay() const {
-        return day;
-    }
-    bool operator<(Date& other) const 
-    {
-        if (year != other.year) return year < other.year;
-        if (month != other.month) return month < other.month;
-        return day < other.day;
-    }
-};
 
 class BitcoinExchange
 {
@@ -56,9 +22,7 @@ class BitcoinExchange
         void processInputFile(std::string inputFile);
         void print()
         {
-            for (std::map<std::string, double>::iterator it = btc_db.begin(); it != btc_db.end(); ++it) {
-            std::cout << it->first << ": " << it->second << std::endl;
-            }
+           
         }
         ~BitcoinExchange();
        
