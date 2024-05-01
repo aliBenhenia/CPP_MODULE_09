586414e090dc29fc61728a04e986b512b0996110
Ali ben <alibenhenia1@gmail.com>
2024-05-01 15:51:53 +0100

    update

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -2,13 +2,13 @@
 
 BitcoinExchange::BitcoinExchange()
 {
-    std::cout << "default constructor" << std::endl;
+    // std::cout << "default constructor" << std::endl;
 }
 
 BitcoinExchange::BitcoinExchange(BitcoinExchange &obj)
 {
     this->btc_db = obj.btc_db;
-    std::cout << "copy constructor" << std::endl;
+    // std::cout << "copy constructor" << std::endl;
 }
 
 BitcoinExchange &BitcoinExchange::operator=(BitcoinExchange &obj)
@@ -20,14 +20,52 @@ BitcoinExchange &BitcoinExchange::operator=(BitcoinExchange &obj)
 // member functions
 void BitcoinExchange :: parseDataBase(std::string db)
 {
+    std::ifstream   fileDb;
+    std::string     line;
+    Date            date; // construct data with -1 
+    double          value;
+    std::string     dateString;
+    size_t          pos;
+
+    fileDb.open(db);
+    if (fileDb.is_open() == 0)
+        throw std::runtime_error("error : can't open a DB file");
+    std::getline(fileDb, line); // ignore header
+    while (std::getline(fileDb, line))
+    {
+        pos = line.find(',');
+        if (pos != std::string::npos) // check is valid pos
+        {
+            dateString = line.substr(0, pos);
+            line.erase(0, pos + 1);
+            pos = dateString.find('-');
+            if (pos != std::string::npos)
+            {
+                date.year = std::atoi(dateString.substr(0, pos).c_str());
+                dateString.erase(0, pos + 1);
+                pos = dateString.find('-');
+                if (pos != std::string::npos)
+                {
+                    date.month = std::atoi(dateString.substr(0 , pos).c_str());
+                    dateString.erase(0, pos + 1);
+                    date.day = std::atoi(dateString.substr(0 , pos).c_str());
+                    value = std::atof(line.c_str());
+                    this->btc_db[date] = value;
+                }
+            }
+        }
+
+    }
+    // at end close it
+    fileDb.close();
     
 }
 void BitcoinExchange :: processInputFile(std::string db)
 {
-    
+    (void)db;
 }
 
 BitcoinExchange::~BitcoinExchange()
 {
-    std::cout << "destructor" << std::endl;
+    // std::cout << "destructor" << std::endl;
 }
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -4,14 +4,38 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <fstream>
 #include <vector>
 #include <stdexcept>// This header is part of the error handling library.
+
+struct Date {
+    int year;
+    int month;
+    int day;
+    Date()
+    {
+        this->year  = -1;
+        this->month = -1;
+        this->day   = -1;
+    }
+    Date(int yr, int mt, int dy)
+    {
+        this->year  = yr;
+        this->month = mt;
+        this->day   = dy;
+    }
+    bool operator<(Date& other) const 
+    {
+        if (year != other.year) return year < other.year;
+        if (month != other.month) return month < other.month;
+        return day < other.day;
+    }
+};
 
 class BitcoinExchange
 {
     private:
-        std::map<std::string, float> btc_db;
-        std::vector<std::string> sd;
+        std::map<Date, double> btc_db;
     public:
         BitcoinExchange();
         BitcoinExchange(BitcoinExchange &obj);
