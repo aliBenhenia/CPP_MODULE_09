d0a898365bcc0f3401b42d4b74d7916fa64e5cdd
Ali ben <alibenhenia1@gmail.com>
2024-05-03 18:47:15 +0100

    init alg exx

---
+++ ex02/PmergeMe.hpp
@@ -0,0 +1,21 @@
+#ifndef RPNEVAL_HPP
+#define RPNEVAL_HPP
+#include <iostream>
+#include <string>
+#include <stack>
+
+class RBN
+{
+    private:
+        std::stack<int> numbers;
+    public:
+        RBN();
+        RBN(const RBN &other);
+        RBN &operator=(const RBN &other);
+        void start(std::string expression);
+        void calculeNumbers(char c);
+        // bool checkIsValid(std::string expression);
+        ~RBN();
+};
+
+#endif
